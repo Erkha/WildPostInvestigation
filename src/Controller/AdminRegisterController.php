5bd83548a2fa559f3364301768121465437d492a
Nathalie Cortès <natyv225@gmail.com>
2019-04-12 11:56:26 +0200

    vers12

--- src/Controller/AdminRegisterController.php
+++ src/Controller/AdminRegisterController.php
@@ -27,34 +27,23 @@ class AdminRegisterController extends AbstractController
         $adminError = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $_POST['username'] = addslashes($_POST['username']);
-            //$_POST['password'] = md5($_POST['password']);
+            $_POST['password'] = md5($_POST['password']);
             $admin = ['username' => $_POST['username'], 'password' => $_POST['password']];
             // verif errors
             $errors= [];
 
             if (!isset($admin['username']) || empty($admin['username'])) {
                 $errors['username']= "invalid username";
-            } else {
-                if (!preg_match("/^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$/", $admin['username'])) {
-                    $errors['username']= "Only letters and numbers allowed";
-                }
-            }
+            } 
             if (!isset($admin['password']) || empty($admin['password'])) {
                 $errors['password']= "invalid password";
-            } else {
-                if (!preg_match(
-                    "/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/",
-                    $admin['password']
-                )) {
-                    $errors['password']= "UpperCase, LowerCase, Number/SpecialChar and min 8 Chars";
-                }
-            }
+            } 
             $adminError = null;
             if (empty($errors)) {
                 $adminManager = new AdminRegisterManager();
                 $adminError = $adminManager->userAdminExist($admin['username'], $admin['password']);
             }
-            if (empty($adminError)) {
+            if (empty($adminError) ) {
                 echo "good";
                 //header('Location: ');
                 //exit();
